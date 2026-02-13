cdb25a1c5ad5a58797f6f48cf4eb9cf05a03c753
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-02-13 17:32:03 +0700

    Create Band-Name-Generator-Project.py

---
+++ Band-Name-Generator-Project.py
@@ -0,0 +1,7 @@
+print("Hi, welcome to my Band Name Generator Project!")
+city_name = input("What is your city name? ")
+city_name = city_name.title()
+pet_name = input('What is your pet name? ')
+pet_name = pet_name.title()
+band_name = city_name + ' ' + pet_name
+print(f'Your Band Name will be: {band_name}')
