b917ba9f8c48a8c12d90b201df0daf6b4839869b
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-02-14 08:55:00 +0700

    Create Day2_Data_types_.py

---
+++ Day2_Data_types_.py
@@ -0,0 +1,11 @@
+# Data Types
+name = 'Gia Bao'
+age = 20
+money = 15.63
+is_student = True
+
+print("--- MY INFO ---")
+print(f'Name: {name}')
+print(f"Age: {age}")
+print(f'Money: {money}$')
+print(f'Am I a student? {is_student}')
