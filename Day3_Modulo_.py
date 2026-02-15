501032009ac41ff89997be3a954e20c47dbfc5dd
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-02-15 18:23:59 +0700

    Create Day3_Modulo_.py
    Implement modulo operator examples and check for odd/even.

---
+++ Day3_Modulo_.py
@@ -0,0 +1,11 @@
+# Modulo Operator
+
+first = 10 % 3
+print(first)   # Output: 1
+
+# How to check Odd or Even using Modulo
+num = float(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
