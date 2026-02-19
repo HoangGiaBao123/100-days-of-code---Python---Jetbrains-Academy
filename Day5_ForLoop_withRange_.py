2d6562cd522aaf50b08aaa167f69cd023d1f2ff5
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-02-19 16:09:54 +0700

    Create Day5_ForLoop_withRange_.py
    Added a Python script demonstrating a for loop with range.

---
+++ Day5_ForLoop_withRange_.py
@@ -0,0 +1,8 @@
+# For loop with Range
+# The combination of the range() function with the Python For Loop allows us to run a loop for as many times as we wish
+# Instead of looping through each item in a List, we can loop through a range of numbers
+
+# Example:
+for num in range(1, 101):  # range() function: range(start_number, end_number - 1)
+    print(num)
+    # Output: 1, 2, 3,......, 98, 99, 100
