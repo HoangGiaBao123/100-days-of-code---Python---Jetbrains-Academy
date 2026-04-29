a3cc4d017b76e45e439e39ee72d29369ebd63866
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-04-29 15:01:46 +0700

    Create Day8_Func_with_multiple_Inputs_.py

---
+++ Day8_Func_with_multiple_Inputs_.py
@@ -0,0 +1,7 @@
+# Function with multiple inputs
+
+def introduce_yourself(name, nation):
+    print(f"Hi, my name's {name}")
+    print(f"I'm from {nation}")
+
+introduce_yourself("Gia Bao", "Vietnam")
